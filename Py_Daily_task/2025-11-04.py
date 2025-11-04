6bb11e6d2b64c03475f5c0256cd9d87cf2bbbcc1
Aravindhan M V <aravindhanmv2008@gmail.com>
2025-11-04 15:58:38 +0530

    Update 2025-11-04.py

--- Py_Daily_task/2025-11-04.py
+++ Py_Daily_task/2025-11-04.py
@@ -27,3 +27,79 @@ print(count)
 
 # Answer :
 
+sentence = "Learn Python Easily"
+result = ""
+
+for char in sentence:
+    if char == " ":
+        result += "-"
+    else:
+        result += char
+
+print(result)
+
+
+# 3. Find Index of an Element (Without using index() or any in-built methods) Write a program to find the index position of a given number manually using loops.
+# python
+# Input:
+# numbers = [11, 22, 33, 44, 55]
+# search = 33
+# Output: 2
+
+
+numbers = [11, 22, 33, 44, 55]
+search = 33
+
+index_position = -1 
+
+for i in range(len(numbers)):
+    if numbers[i] == search:
+        index_position = i
+        break
+
+print(index_position)
+
+
+
+# You are given two lists:
+# One list shows the gender of each student ('M' for male, 'F' for female).
+# The other list shows their marks in the same order.
+# Write a Python program to:
+# Find the average marks of male students.
+# Find the average marks of female students.
+# Print "Male <average>" if male students have the higher average, or "Female <average>" if female students have the higher average.
+
+# For example:
+# Test
+# find_higher_average(['M','F','M','F','M','F','M'],
+#                     [81.5, 70.0, 98.5, 60.0, 75.0, 50.0, 85.0]) 
+
+# Output: Male 85.0
+
+# find_higher_average(['M','F','M','F','M','F','M'],
+#                     [59.5, 80.0, 61.0, 79.0, 60.5, 81.0, 60.0])
+
+# Output: Female 80.0
+
+
+def find_higher_average(gen_list, marks_list):
+    male_sum = 0
+    female_sum = 0
+    male_count = 0
+    female_count = 0
+
+    for i in range(len(gen_list)):
+        if gen_list[i] == 'M':
+            male_sum += marks_list[i]
+            male_count += 1
+        else:
+            female_sum += marks_list[i]
+            female_count += 1
+
+    male_avg = male_sum / male_count
+    female_avg = female_sum / female_count
+
+    if male_avg > female_avg:
+        print("Male", male_avg)
+    else:
+        print("Female", female_avg)
