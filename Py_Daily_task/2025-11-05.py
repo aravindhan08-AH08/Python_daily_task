9bbee37ff0024c25d615a7485166984da2fbc56b
Aravindhan M V <aravindhanmv2008@gmail.com>
2025-11-05 17:12:21 +0530

    Update 2025-11-05.py

--- Py_Daily_task/2025-11-05.py
+++ Py_Daily_task/2025-11-05.py
@@ -35,3 +35,15 @@ users = {
 login_system(users, "alice", "pass123")     # Output: Login Successful
 login_system(users, "bob", "wrongpass")     # Output: Invalid Password
 login_system(users, "david", "anything")    # Output: User Not Found
+
+
+# 2. Product Inventory Manager
+# You have a product inventory stored as a dictionary:
+#  Keys are product names, and values are their quantities.
+# Write a program to:
+# Add a new product or update quantity if it already exists.
+# Print the updated inventory.
+# Example:
+# inventory = {"apple": 10, "banana": 5, "mango": 8}
+# If input is "apple 4", total becomes 14.
+#  If input is "grape 6", add it as a new product.
