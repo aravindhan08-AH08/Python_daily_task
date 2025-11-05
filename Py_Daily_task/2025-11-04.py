969f9c4f85bf09704d6b7647766b8da626f5ee8b
Aravindhan M V <aravindhanmv2008@gmail.com>
2025-11-05 14:18:48 +0530

    Some error issues

--- Py_Daily_task/2025-11-04.py
+++ Py_Daily_task/2025-11-04.py
@@ -1,108 +1,108 @@
-# # Given two lists — one with last month’s marks and another with this month’s marks.
-# # print how many students improved their scores.
-# # Input:
-# # last_month_score = [45, 60, 70, 55, 80]
-# # this_month_score = [50, 58, 75, 65, 78]
-# # Output: 3
-# # Explanation: here students number 1, 3, and 4 are improved in the scores
-
-# # Answer :
-
+# Given two lists — one with last month’s marks and another with this month’s marks.
+# print how many students improved their scores.
+# Input:
 # last_month_score = [45, 60, 70, 55, 80]
 # this_month_score = [50, 58, 75, 65, 78]
+# Output: 3
+# Explanation: here students number 1, 3, and 4 are improved in the scores
 
-# count = 0
+# Answer :
 
-# for i in range(len(last_month_score)):
-#     if this_month_score[i] > last_month_score[i]:
-#         count += 1
+last_month_score = [45, 60, 70, 55, 80]
+this_month_score = [50, 58, 75, 65, 78]
 
-# print(count)
+count = 0
 
-# # 2. Convert all spaces in a given sentence into - (without using in-built functions).
-# # ```python
-# # Input: "Learn Python Easily"
-# # Output: "Learn-Python-Easily"
-# # ```
+for i in range(len(last_month_score)):
+    if this_month_score[i] > last_month_score[i]:
+        count += 1
 
-# # Answer :
+print(count)
 
-# sentence = "Learn Python Easily"
-# result = ""
+# 2. Convert all spaces in a given sentence into - (without using in-built functions).
+# ```python
+# Input: "Learn Python Easily"
+# Output: "Learn-Python-Easily"
+# ```
 
-# for char in sentence:
-#     if char == " ":
-#         result += "-"
-#     else:
-#         result += char
+# Answer :
 
-# print(result)
+sentence = "Learn Python Easily"
+result = ""
 
+for char in sentence:
+    if char == " ":
+        result += "-"
+    else:
+        result += char
 
-# # 3. Find Index of an Element (Without using index() or any in-built methods) Write a program to find the index position of a given number manually using loops.
-# # python
-# # Input:
-# # numbers = [11, 22, 33, 44, 55]
-# # search = 33
-# # Output: 2
+print(result)
 
 
+# 3. Find Index of an Element (Without using index() or any in-built methods) Write a program to find the index position of a given number manually using loops.
+# python
+# Input:
 # numbers = [11, 22, 33, 44, 55]
 # search = 33
-
-# index_position = -1 
-
-# for i in range(len(numbers)):
-#     if numbers[i] == search:
-#         index_position = i
-#         break
-
-# print(index_position)
+# Output: 2
 
 
+numbers = [11, 22, 33, 44, 55]
+search = 33
 
-# # You are given two lists:
-# # One list shows the gender of each student ('M' for male, 'F' for female).
-# # The other list shows their marks in the same order.
-# # Write a Python program to:
-# # Find the average marks of male students.
-# # Find the average marks of female students.
-# # Print "Male <average>" if male students have the higher average, or "Female <average>" if female students have the higher average.
+index_position = -1 
 
-# # For example:
-# # Test
-# # find_higher_average(['M','F','M','F','M','F','M'],
-# #                     [81.5, 70.0, 98.5, 60.0, 75.0, 50.0, 85.0]) 
+for i in range(len(numbers)):
+    if numbers[i] == search:
+        index_position = i
+        break
 
-# # Output: Male 85.0
-
-# # find_higher_average(['M','F','M','F','M','F','M'],
-# #                     [59.5, 80.0, 61.0, 79.0, 60.5, 81.0, 60.0])
-
-# # Output: Female 80.0
+print(index_position)
 
 
-# def find_higher_average(gen_list, marks_list):
-#     male_sum = 0
-#     female_sum = 0
-#     male_count = 0
-#     female_count = 0
 
-#     for i in range(len(gen_list)):
-#         if gen_list[i] == 'M':
-#             male_sum += marks_list[i]
-#             male_count += 1
-#         else:
-#             female_sum += marks_list[i]
-#             female_count += 1
+# You are given two lists:
+# One list shows the gender of each student ('M' for male, 'F' for female).
+# The other list shows their marks in the same order.
+# Write a Python program to:
+# Find the average marks of male students.
+# Find the average marks of female students.
+# Print "Male <average>" if male students have the higher average, or "Female <average>" if female students have the higher average.
 
-#     male_avg = male_sum / male_count
-#     female_avg = female_sum / female_count
+# For example:
+# Test
+# find_higher_average(['M','F','M','F','M','F','M'],
+#                     [81.5, 70.0, 98.5, 60.0, 75.0, 50.0, 85.0]) 
 
-#     if male_avg > female_avg:
-#         print("Male", male_avg)
-#     else:
-#         print("Female", female_avg)
+# Output: Male 85.0
+
+# find_higher_average(['M','F','M','F','M','F','M'],
+#                     [59.5, 80.0, 61.0, 79.0, 60.5, 81.0, 60.0])
+
+# Output: Female 80.0
+
+
+def find_higher_average(gen_list, marks_list):
+    male_sum = 0
+    female_sum = 0
+    male_count = 0
+    female_count = 0
+
+    for i in range(len(gen_list)):
+        if gen_list[i] == 'M':
+            male_sum += marks_list[i]
+            male_count += 1
+        else:
+            female_sum += marks_list[i]
+            female_count += 1
+
+    male_avg = male_sum / male_count
+    female_avg = female_sum / female_count
+
+    if male_avg > female_avg:
+        print("Male", male_avg)
+    else:
+        print("Female", female_avg)
 
 # Question 1:
 # Write a Python program to check if a given string is a Pangram or not.
@@ -124,17 +124,17 @@
 # The input string is missing some letters (‘l’, ‘z’, and ‘y’), so it is not a Pangram.
 
 
-sentence = input("Enter one sentence: ")
-sentence = sentence.lower()
-words  = "abcdefghijklmnopqrstuvwxyz"
-count=0
-for i in words:
-    if i in sentence:
-        count+=1
-if count==26:
-    print(True)
-else:
-    print(False)        
+# sentence = input("Enter one sentence: ")
+# sentence = sentence.lower()
+# words  = "abcdefghijklmnopqrstuvwxyz"
+# count=0
+# for i in words:
+#     if i in sentence:
+#         count+=1
+# if count==26:
+#     print(True)
+# else:
+#     print(False)        
 
 
      
--- Py_Daily_task/2025-11-05.py
+++ Py_Daily_task/2025-11-05.py
@@ -1,3 +1,20 @@
+# 1. User Login System
+# You’re building a simple login system.
+#  User credentials are stored in a dictionary where the key is the username and the value is the password.
+#  Write a function that:
+# Takes username and password as input.
+# Checks if the user exists and if the password matches.
+# Prints:
+# "Login Successful" if both match.
+# "Invalid Password" if username exists but password is wrong.
+# "User Not Found" if username doesn’t exist.
+# Example Data:
+# users = {
+#     "alice": "pass123",
+#     "bob": "qwerty",
+#     "charlie": "hello123"
+# }
+
 def login_system(users, username, password):
     if username in users:
         if users[username] == password:
@@ -8,7 +25,6 @@ def login_system(users, username, password):
         print("User Not Found")
 
 
-# Example data
 users = {
     "alice": "pass123",
     "bob": "qwerty",
